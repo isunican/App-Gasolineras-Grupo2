48a8cd46b84f3dd6fabc9479b01245ec344b639a
Aitor Angulo Salas <aitor001970@gmail.com>
2024-10-30 18:19:46 +0100

    Creados los test de la calse GasolineraAdapter

---
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/activities/main/GasolinerasArrayAdapterTest.java
@@ -0,0 +1,22 @@
+package es.unican.gasolineras.activities.main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GasolinerasArrayAdapterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void procesaHorarioTest() {
+        
+    }
+
+    @Test
+    public void gasolineraAbierta(){
+
+    }
+}
