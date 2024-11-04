7c497a694252b685d750fe59383c21270e057c0b
oscarrzz <150368505+oscarrzz@users.noreply.github.com>
2024-11-04 18:27:33 +0100

    Plan de pruebas e implementación finalizados

--- AndroidProject/app/src/test/java/es/unican/gasolineras/activities/main/MainPresenterITest.java
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/activities/main/MainPresenterITest.java
@@ -1,27 +1,37 @@
 package es.unican.gasolineras.activities.main;
 
-import static org.junit.Assert.assertEquals;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.Arrays;
 import java.util.List;
 
 import es.unican.gasolineras.model.Gasolinera;
+import es.unican.gasolineras.model.PuntoInteres;
 import es.unican.gasolineras.model.TipoCombustible;
 import es.unican.gasolineras.repository.ICallBack;
 import es.unican.gasolineras.repository.IGasolinerasRepository;
+import es.unican.gasolineras.repository.IPuntosInteresDAO;
 
-public class FiltrarPorPrecioMaximoCombustibleTest {
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class MainPresenterITest {
+
+    private GasolineraDistanciaComparator comparadorDistancia;
+
+    private Gasolinera cercana;
+    private Gasolinera lejana;
+    private Gasolinera auxCercana;
 
     @Mock
     private static IGasolinerasRepository mockGasolineras;
@@ -38,13 +48,51 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
 
     private List<Gasolinera> listaGasolineras;
 
+    @Mock
+    private static IPuntosInteresDAO mockPuntoInteres;
+
+    private Gasolinera gasolineraCercana;
+    private Gasolinera gasolineraNeutra;
+    private Gasolinera gasolineraLejana;
+    private Gasolinera gasolineraMuylejana;
+
+    private PuntoInteres universidad;
+
+
     @Before
-    public void inicializa() {
+    public void inicializa(){
 
         // Inicializo los mocks
         MockitoAnnotations.openMocks(this);
 
-        // Creo las gasolineras con precios ascendentes
+        //creo gasolineras necesarias para DistanciaComparatorTest
+
+        cercana = new Gasolinera();
+        cercana.setId("GasolineraPaco");
+        cercana.setDireccion("Sardinero");
+        cercana.setLatitud(43.47618775921668);
+        cercana.setLongitud(-3.7933535145721233);
+
+        lejana = new Gasolinera();
+        lejana.setId("GasolineraMario");
+        lejana.setDireccion("Torrelavega");
+        lejana.setLatitud(43.356608665447474);
+        lejana.setLongitud(-4.046146566530483);
+
+        auxCercana = new Gasolinera();
+        auxCercana.setId("GasolineraJaime");
+        auxCercana.setLatitud(43.47618775921668);
+        auxCercana.setLongitud(-3.7933535145721233);
+
+        //creo un punto de interes
+        universidad = new PuntoInteres();
+        universidad.nombre = "Universidad";
+        universidad.latitud = 43.47122194796555;
+        universidad.longitud = -3.800797786772268;
+
+        comparadorDistancia = new GasolineraDistanciaComparator(universidad);
+
+        // Creo las gasolineras con precios ascendentes para Filtrar por precio maximo
         gasolinera1 = new Gasolinera();
         gasolinera1.setGasoleoA(1.053);
         gasolinera1.setGasolina98E5(1.153);
@@ -66,16 +114,45 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
         // Añado las gasolineras a la lista
         listaGasolineras = Arrays.asList(gasolinera1, gasolinera2, gasolinera3, gasolinera4);
 
+        //creo las gasolineras para OrdenarGasolinerasCercanasTest
+        gasolineraCercana = new Gasolinera();
+        gasolineraCercana.setId("GasolineraPaco");
+        gasolineraCercana.setDireccion("Sardinero");
+        gasolineraCercana.setLatitud(43.47618775921668);
+        gasolineraCercana.setLongitud(-3.7933535145721233);
+
+        gasolineraLejana = new Gasolinera();
+        gasolineraLejana.setId("GasolineraMario");
+        gasolineraLejana.setDireccion("Torrelavega");
+        gasolineraLejana.setLatitud(43.356608665447474);
+        gasolineraLejana.setLongitud(-4.046146566530483);
+
+        gasolineraNeutra = new Gasolinera();
+        gasolineraNeutra.setId("GasolineraLuis");
+        gasolineraNeutra.setDireccion("Bezana");
+        gasolineraNeutra.setLatitud(43.406608665447474);
+        gasolineraNeutra.setLongitud(-4.0);
+
+        gasolineraMuylejana= new Gasolinera();
+        gasolineraMuylejana.setId("GasolineraJuan");
+        gasolineraMuylejana.setDireccion("America");
+        gasolineraMuylejana.setLatitud(0.0);
+        gasolineraMuylejana.setLongitud(0.0);
+
+
+        when(mockPuntoInteres.loadByName("Universidad")).thenReturn(universidad);
+        
         sut = new MainPresenter();
     }
 
     @Test
-    public void testFiltraGasolinerasPorPrecioMaximoLimiteBajo() {
+    public void testFiltraYOrdenaGasolinerasPorPrecioMaximoYDistanciaLimiteBajo() {
         double precioMaximo = 1.06;
 
+        // Mockeo de gasolineras y llamada al repositorio
         doAnswer(invocation -> {
             ICallBack callBack = invocation.getArgument(0);
-            callBack.onSuccess(listaGasolineras);
+            callBack.onSuccess(listaGasolineras); // Lista de gasolineras definida en la prueba
             return null;
         }).when(mockGasolineras).requestGasolineras(any(ICallBack.class), any(String.class));
 
@@ -83,10 +160,13 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
 
         ArgumentCaptor<List<Gasolinera>> captor = ArgumentCaptor.forClass(List.class);
 
+        // Inicializo y aplico filtros de precio y distancia
         sut.init(mockVista);
         sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLEO_A);
+        sut.ordenarGasolinerasCercanasPtoInteres(universidad);
 
-        verify(mockVista, times(2)).showStations(captor.capture());
+        // Verifico que se muestre la lista filtrada y ordenada
+        verify(mockVista, times(3)).showStations(captor.capture());
 
         List<Gasolinera> gasolinerasFiltradas = captor.getValue();
         assertEquals(1, gasolinerasFiltradas.size());
@@ -94,7 +174,7 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
     }
 
     @Test
-    public void testFiltraGasolinerasPorPrecioMaximoLimiteAlto() {
+    public void testFiltraYOrdenaGasolinerasPorPrecioMaximoYDistanciaLimiteAlto() {
         double precioMaximo = 1.26;
 
         doAnswer(invocation -> {
@@ -109,8 +189,9 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
 
         sut.init(mockVista);
         sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLINA_98_E5);
+        sut.ordenarGasolinerasCercanasPtoInteres(universidad);
 
-        verify(mockVista, times(2)).showStations(captor.capture());
+        verify(mockVista, times(3)).showStations(captor.capture());
 
         List<Gasolinera> gasolinerasFiltradas = captor.getValue();
         assertEquals(3, gasolinerasFiltradas.size());
@@ -120,7 +201,7 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
     }
 
     @Test
-    public void testFiltraGasolinerasPorPrecioMaximoLimiteIgual() {
+    public void testFiltraYOrdenaGasolinerasPorPrecioMaximoYDistanciaLimiteIgual() {
         double precioMaximo = 1.203;
 
         doAnswer(invocation -> {
@@ -135,8 +216,9 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
 
         sut.init(mockVista);
         sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLINA_98_E5);
+        sut.ordenarGasolinerasCercanasPtoInteres(universidad);
 
-        verify(mockVista, times(2)).showStations(captor.capture());
+        verify(mockVista, times(3)).showStations(captor.capture());
 
         List<Gasolinera> gasolinerasFiltradas = captor.getValue();
         assertEquals(2, gasolinerasFiltradas.size());
@@ -145,8 +227,8 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
     }
 
     @Test
-    public void testFiltraGasolinerasPorPrecioMaximoSinCombustible() {
-        double precioMaximo = 1.5;
+    public void testFiltraYOrdenaGasolinerasPorPrecioMaximoYDistanciaSinResultados() {
+        double precioMaximo = 0.5;
 
         doAnswer(invocation -> {
             ICallBack callBack = invocation.getArgument(0);
@@ -159,41 +241,17 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
         ArgumentCaptor<List<Gasolinera>> captor = ArgumentCaptor.forClass(List.class);
 
         sut.init(mockVista);
-        sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.HIDROGENO);
+        sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLEO_A);
+        sut.ordenarGasolinerasCercanasPtoInteres(universidad);
 
-        verify(mockVista, times(2)).showStations(captor.capture());
-
-        List<Gasolinera> gasolinerasFiltradas = captor.getValue();
-        assertEquals(2, gasolinerasFiltradas.size());
-        assertEquals(gasolinera3, gasolinerasFiltradas.get(0));
-        assertEquals(gasolinera4, gasolinerasFiltradas.get(1));
-    }
-
-    @Test
-    public void testFiltraGasolinerasPrecioMuyBajo() {
-        double precioMaximo = 0.001;
-
-        doAnswer(invocation -> {
-            ICallBack callBack = invocation.getArgument(0);
-            callBack.onSuccess(listaGasolineras);
-            return null;
-        }).when(mockGasolineras).requestGasolineras(any(ICallBack.class), any(String.class));
-
-        when(mockVista.getGasolinerasRepository()).thenReturn(mockGasolineras);
-
-        ArgumentCaptor<List<Gasolinera>> captor = ArgumentCaptor.forClass(List.class);
-
-        sut.init(mockVista);
-        sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLINA_98_E5);
-
-        verify(mockVista, times(2)).showStations(captor.capture());
+        verify(mockVista, times(3)).showStations(captor.capture());
 
         List<Gasolinera> gasolinerasFiltradas = captor.getValue();
         assertEquals(0, gasolinerasFiltradas.size());
     }
 
     @Test
-    public void testFiltraGasolinerasPrecioMuyAlto() {
+    public void testFiltraYOrdenaGasolinerasPorPrecioMaximoMuyAlto() {
         double precioMaximo = 10.0;
 
         doAnswer(invocation -> {
@@ -207,9 +265,10 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
         ArgumentCaptor<List<Gasolinera>> captor = ArgumentCaptor.forClass(List.class);
 
         sut.init(mockVista);
-        sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLINA_98_E5);
+        sut.filtraGasolinerasPorPrecioMaximo(precioMaximo, TipoCombustible.GASOLEO_A);
+        sut.ordenarGasolinerasCercanasPtoInteres(universidad);
 
-        verify(mockVista, times(2)).showStations(captor.capture());
+        verify(mockVista, times(3)).showStations(captor.capture());
 
         List<Gasolinera> gasolinerasFiltradas = captor.getValue();
         assertEquals(4, gasolinerasFiltradas.size());
@@ -218,5 +277,4 @@ public class FiltrarPorPrecioMaximoCombustibleTest {
         assertEquals(gasolinera3, gasolinerasFiltradas.get(2));
         assertEquals(gasolinera4, gasolinerasFiltradas.get(3));
     }
-
 }
