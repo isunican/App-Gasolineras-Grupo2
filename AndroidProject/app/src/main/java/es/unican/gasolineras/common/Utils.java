671bb25509132e2afdee08c0a2566426e06c0b72
Aitor Angulo Salas <71783846+HERRERA99@users.noreply.github.com>
2024-11-08 00:33:56 +0100

    Revert "Feature/506399 mostrar horario gasolinera"

--- AndroidProject/app/src/main/java/es/unican/gasolineras/common/Utils.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/common/Utils.java
@@ -11,8 +11,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
 import es.unican.gasolineras.model.Gasolinera;
